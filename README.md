c3e24114bc9701f081a4e01245f1a7d9917e5f3a
Everett Toews <everett.toews@rackspace.com>
2017-08-27 14:28:06 -0500

    Preview script

--- README.md
+++ README.md
@@ -2,17 +2,6 @@
 
 Preview
 
-```
-unset ${!DOCKER_*}
-
-docker pull grahamc/jekyll
-
-docker run --rm -it \
-  --name jekyll \
-  --volume=$(pwd):/src \
-  --publish 4000:4000 \
-  grahamc/jekyll \
-  serve --watch --future --drafts --force_polling --host 0.0.0.0
-
-open http://localhost:4000
+```bash
+./preview.sh
 ```
---
+++ preview.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+unset ${!DOCKER_*}
+
+docker pull grahamc/jekyll
+
+docker run --rm --detach \
+  --name jekyll \
+  --volume=$(pwd):/src \
+  --publish 4000:4000 \
+  grahamc/jekyll \
+  serve --watch --future --drafts --force_polling --host 0.0.0.0
+
+echo -n "Waiting for jekyll to start"
+
+until curl -s -q http://localhost:4000 > /dev/null; do
+    sleep 1
+    echo -n "."
+done
+
+echo ""
+
+open http://localhost:4000
+
+read -p "Press any key to quit preview and kill container. "
+
+docker rm -f jekyll
